83516bf0909eb57ea8ef412eaa842a33f67a952a
roberto <robertoramirezmoreno@gmail.com>
2023-07-05 18:32:46 +0200

    routes fixed

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,12 @@ use App\Http\Controllers\HomeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+Route::get('/ad', [AdController::class, 'showAds'])->name('ads');
 Route::get('/ad/create', [AdController::class, 'create'])->name('create');
 Route::post('/ad', [AdController::class, 'store'])->name('ad.store');
 
 //Route::post('/ad/ads', [AdController::class, 'store'])->name('ads.store');
-Route::get('/ad', [AdController::class, 'showAds'])->name('ads');
 
-Auth::routes();
+Auth::routes(    
+);
 
