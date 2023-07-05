df08ab25254599f26873223e968f5a0b572b54e2
roberto <robertoramirezmoreno@gmail.com>
2023-07-05 13:32:35 +0200

    improvements added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,9 @@
 <x-layout>
     <x-slot name='title'>DecorationHome - Homepage</x-slot>
     <h1>Bienvenido a DecorationHome.com</h1>
+    @if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
 </x-layout>
