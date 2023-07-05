516df083f06ca73f850b9897832bf1fd328f326a
roberto <robertoramirezmoreno@gmail.com>
2023-07-05 16:35:46 +0200

    rename proyect homedecoration.com

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
-    <x-slot name='title'>DecorationHome - Homepage</x-slot>
-    <h1>Bienvenido a DecorationHome.com</h1>
+    <x-slot name='title'>HomeDecoration - Homepage</x-slot>
+    <h1>Bienvenido a HomeDecoration.com</h1>
     @if(session('success'))
     <div class="alert alert-success">
         {{ session('success') }}
