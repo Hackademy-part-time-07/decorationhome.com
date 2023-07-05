efdc522546007db1cccca2ac65e9e9bf4390880f
roberto <robertoramirezmoreno@gmail.com>
2023-07-05 18:27:53 +0200

    routes fixed

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,13 @@ use App\Http\Controllers\AdController;
 use App\Http\Livewire\CreateAd;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('home');
-});
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/ad/create', [AdController::class, 'create'])->name('create');
 Route::post('/ad', [AdController::class, 'store'])->name('ad.store');
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::post('/ad/ads', [AdController::class, 'store'])->name('ads.store');
+//Route::post('/ad/ads', [AdController::class, 'store'])->name('ads.store');
 Route::get('/ad', [AdController::class, 'showAds'])->name('ads');
 
 Auth::routes();
