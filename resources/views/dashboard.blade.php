34d35f5c53ea36e751440059d199d43494549043
roberto <robertoramirezmoreno@gmail.com>
2023-07-13 15:51:08 +0200

    bg

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,7 +10,7 @@
 
 
     <div class="container mx-auto p-4">
-        <h1 class="text-2xl font-bold mb-4">Administrador</h1>
+        <h1 class="text-2xl font-bold mb-4" style="margin-top: 30px;">Administrador</h1>
 
         <form action="{{ route('dashboard') }}" method="GET" class="mb-4">
             <div class="flex">
